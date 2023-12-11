8015ee8078ddc7e34a8cbe9440536935c7a2407f
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2023-12-11 22:57:03 +0530

    Started Work on front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
 
 function App() {
 
-  return <div className="App"></div>
+  return (
+  <Router>
+    <Header></Header>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+</Router>
+  )
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>
+        <div>To Do App</div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Login</li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
