e55e823217ebacbf57a54071d5c5b3f2d225e258
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2023-12-13 20:18:33 +0530

    Designed Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <section>
+        <form>
+          <h2>Login</h2>
+          <input type='email' placeholder='Email'/>
+          <input type='password' placeholder='Password'/>
+          <button type='submit'>Login</button>
+          <h4>Or</h4>
+          <Link className='registerButton' to={'/register'}>Register</Link>
+        </form>
+      </section>
+    </div>
   )
 }
 
