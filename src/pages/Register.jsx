19ef657040c3f45a567131a50a6505fa2944c2e5
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2023-12-14 10:31:49 +0530

    Created Register Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='login'>
+      <section>
+        <form>
+          <h2>Register</h2>
+          <input type='text' placeholder='Name'/>
+          <input type='email' placeholder='Email'/>
+          <input type='password' placeholder='Password'/>
+          <button type='submit'>Submit</button>
+          <h4>Or</h4>
+          <Link className='registerButton' to={'/login'}>Login</Link>
+        </form>
+      </section>
+    </div>
   )
 }
 
