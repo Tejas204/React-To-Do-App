455b2110eab41e77edc9ab25f31909a5d14fbe43
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2023-12-19 16:31:03 +0530

    Added isAuthenticated flag to App Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/app.scss'
+import {createContext} from "react"
 
+// Server URI
 export const server = "https://nodejs-todo-app-8f77.onrender.com/api/v1"
 
+// Create context: isAuthenticated
+export const Context = createContext({isAuthenticated: false});
+
+/* 
+* Component: AppWrapper
+* Functionality: Wraps the App component and uses hooks to set isAuthenticated
+*/
+const AppWrapper = () => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return(
+    <Context.Provider
+      value={{
+        isAuthenticated, 
+        setIsAuthenticated
+      }}>
+      <App />
+    </Context.Provider>
+  );
+}
+
+/*
+ * Component: Root component
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWrapper/>
   </React.StrictMode>,
 )
