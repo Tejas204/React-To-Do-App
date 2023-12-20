76b02936aad4ea561810d7588e96d2f7e2804d9e
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2023-12-20 22:29:43 +0530

    Implemented login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,73 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Context } from '../main';
+import toast from 'react-hot-toast';
 
 const Login = () => {
+
+  // Hook: Set and access isAuthenticated
+  const {isAuthenticated, setIsAuthenticated} = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  // Function: Handle Submit Action
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const {data} = await axios.post(
+      `${server}/users/login`, 
+      {
+        email: email, 
+        password: password,
+      }, 
+      {
+        headers:{
+          "Content-Type": "application/json",
+        },
+        withCredentials: true
+      }
+    );
+      
+    toast.success(data.message, {
+      style: {
+        borderRadius: '10px',
+        fontFamily: 'sans-serif',
+      },
+    });
+
+    // Registration is success
+    setIsAuthenticated(true);
+      
+    } catch (error) {
+      toast.error("Error");
+      console.log(error);
+
+      // Registration is failure
+      setIsAuthenticated(false);
+    }
+  }
+
   return (
     <div className='login'>
       <section>
-        <form>
+        <form onSubmit={submitHandler}>
           <h2>Login</h2>
-          <input type='email' placeholder='Email'/>
-          <input type='password' placeholder='Password'/>
+          <input 
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)} 
+            type='email' 
+            placeholder='Email'/>
+
+          <input 
+            value={password} 
+            required
+            onChange={(e) => setPassword(e.target.value)} 
+            type='password' 
+            placeholder='Password'/>
+
           <button type='submit'>Login</button>
           <h4>Or</h4>
           <Link className='registerLoginButton' to={'/register'}>Register</Link>
