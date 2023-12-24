aa2ff33a082c751e7d0c1f6945569277121206df
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2023-12-24 17:37:07 +0530

    Added profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../main'
+import Loader from '../components/Loader';
 
 const Profile = () => {
+
+  const {setIsAuthenticated, user, loading} = useContext(Context);
+  console.log(user);
   return (
-    <div>Profile</div>
-  )
-}
+
+    loading ? <Loader/> : ( 
+    <div>
+      <h1>{user?.name}</h1>
+      <h1>{user?.email}</h1>
+    </div>
+      
+    )
+    
+  );
+};
 
 export default Profile
